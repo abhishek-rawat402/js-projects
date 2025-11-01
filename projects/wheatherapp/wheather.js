f23d8796776b18d7fcbbde2e74c1b64db8174ed4
abhishek-rawat402 <abhishekrawat1409@gmail.com>
2025-11-01 23:33:41 +0530

    deleted the wheather folder

--- projects/wheatherapp/wheather.js
+++
@@ -1,7 +0,0 @@
-let p = document.createElement("div")
-p.innerHTML = "<h1>hello guys </h1>";
-document.body.appendChild(p);
-
-document.getElementById("st").addEventListener('click',function(){
-    document.getElementById("st").appendChild(p);
-})
